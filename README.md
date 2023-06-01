6f0b3282d8f61c16e147691fe2a46d46ed581a81
NorbertPiecka <norbert.piecka9@gmail.com>
2023-06-01 18:48:33 +0200

    Add metrics for java app

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 1. Write down your ip in prometheus.yml
 2. Run: docket compose up
-3. Start python app: py hello_world.py
+3. pip install prometheus_client
+4. Start python app: py hello_world.py -> Port 8000
+5. Start java-app in Intelij -> Port 8002
+6. Promethues data localhost:9090/targets
--- SimpleLogginApp/src/main/java/pl/edu/pw/SimpleLoggingService.java
+++ SimpleLogginApp/src/main/java/pl/edu/pw/SimpleLoggingService.java
@@ -4,17 +4,25 @@ import org.apache.commons.lang3.RandomUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.util.Objects;
 import java.util.Random;
 import java.util.Scanner;
+import io.prometheus.client.exporter.HTTPServer;
+import io.prometheus.client.Counter;
 
 public class SimpleLoggingService {
 
     private static final Logger logger = LoggerFactory.getLogger(SimpleLoggingService.class);
+    private static final Counter requestsCounter = Counter.build()
+            .name("myapp_requests_total")
+            .help("Total number of requests processed")
+            .register();
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String state;
+        HTTPServer server = new HTTPServer(8002);
         do {
             int wordLength = RandomUtils.nextInt(3, 12);
             byte[] array = new byte[wordLength];
@@ -37,10 +45,11 @@ public class SimpleLoggingService {
             }
 
             System.out.printf("Generated word: %s%nLogging level: %s%n", generatedString, level);
-
+            requestsCounter.inc();
             Scanner scanner = new Scanner(System.in);
             state = scanner.nextLine();
         } while (!Objects.equals(state, "stop"));
+        server.close();
     }
 
     private enum LoggingLevel {
